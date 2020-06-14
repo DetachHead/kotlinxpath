f1758ca4b169b690b52af85753eca5ba7a5126aa
DetachHead <detachhead@users.noreply.github.com>
2020-06-14 17:57:03 +1000

    fix formatting

--- src/commonTest/kotlin/basictest.kt
+++ src/commonTest/kotlin/basictest.kt
@@ -15,7 +15,7 @@ class test {
 
     @Test
     fun test2() {
-        assertEquals("//div/p[@class='asdf']", xpath{
+        assertEquals("//div/p[@class='asdf']", xpath {
             descendantOrSelf {
                 //TODO: fix wacky global xpath string. currently the first string needs to be invoked before the divider override because of the order it appends to the string
                 "div"() / "p"(mapOf("class" to "asdf"))
