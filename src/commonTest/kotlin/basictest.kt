e554da519564d4149d30d1b69db0ff4b878b4101
DetachHead <detachhead@users.noreply.github.com>
2020-06-14 17:45:08 +1000

    made strings invokable to add elements & added divide operator overload

--- src/commonTest/kotlin/basictest.kt
+++ src/commonTest/kotlin/basictest.kt
@@ -6,9 +6,19 @@ class test {
     fun test1() {
         assertEquals("//div[@id='thing' and normalize-space(.)='sdfg']", xpath {
             descendantOrSelf {
-                div(attributes = mapOf("id" to "thing")) {
+                "div"(attributes = mapOf("id" to "thing")) {
                     +"sdfg"
                 }
+            }
+        }.toString())
+    }
+
+    @Test
+    fun test2() {
+        assertEquals("//div/p[@class='asdf']", xpath{
+            descendantOrSelf {
+                //TODO: fix wacky global xpath string. currently the first string needs to be invoked before the divider override because of the order it appends to the string
+                "div"() / "p"(mapOf("class" to "asdf"))
             }
         }.toString())
     }
