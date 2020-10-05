266b137930991abc8dbc8bfab2de1882cf794277
DetachHead <detachhead@users.noreply.github.com>
2020-10-05 23:08:05 +1000

    finish the operator infix functions, add functionExpression

--- src/commonMain/kotlin/Expressions.kt
+++ src/commonMain/kotlin/Expressions.kt
@@ -21,12 +21,13 @@ public class XpathString(public val value: String) {
     }
 }
 
-/** two [Expression]s combined together by an [Operator] */
-public class OperatorExpression(
-    public val first: Expression,
-    public val operator: Operator,
-    public val second: Expression
-) : Expression("$first $operator $second")
+/** combines two [Expression]s together with an [Operator] */
+private fun operatorExpression(first: Expression, operator: Operator, second: Expression): Expression =
+    Expression("$first $operator $second")
+
+/** creates an expression with an xpath [function call](https://en.wikipedia.org/wiki/XPath#Functions_and_operators) */
+private fun functionExpression(name: String, args: List<Expression?> = listOf()): Expression =
+    Expression("$name(${args.filterNotNull().joinToString(",")})")
 
 /**
  * typesafe builder for [Expression] for use as [predicates](https://en.wikipedia.org/wiki/XPath#Predicates)
@@ -35,12 +36,70 @@ public class PredicateBuilder : Buildable<Expression> {
     private lateinit var predicate: Expression
     override fun build(): Expression = predicate
 
-    public fun String.compare(operator: ComparisonOperator, other: String): Expression = OperatorExpression(
-        Expression(this), operator, Expression(XpathString(other))
-    ).also { predicate = it }
+    private fun Expression.operator(
+        operator: Operator,
+        other: Expression
+    ): Expression = operatorExpression(this, operator, other).also { predicate = it }
 
-    /**
-     * creates a predicate checking the equality of an Xpath
+    /* Xpath operators */
+    //=:
+    public infix fun Expression.equal(other: Expression): Expression = this.operator(ComparisonOperator.`=`, other)
+    public infix fun Expression.equal(other: String): Expression = this equal Expression(XpathString(other))
+    public infix fun String.equal(other: Expression): Expression = Expression(this) equal other
+    public infix fun String.equal(other: String): Expression = Expression(this) equal other
+
+    //!=:
+    public infix fun Expression.not(other: Expression): Expression = this.operator(ComparisonOperator.`=`, other)
+    public infix fun Expression.not(other: String): Expression = this not Expression(XpathString(other))
+    public infix fun String.not(other: Expression): Expression = Expression(this) not other
+    public infix fun String.not(other: String): Expression = Expression(this) not other
+
+    //>:
+    public infix fun Expression.greaterThan(other: Expression): Expression = operator(ComparisonOperator.greater, other)
+    public infix fun Expression.greaterThan(other: Int): Expression = greaterThan(Expression(other))
+    public infix fun Int.greaterThan(other: Expression): Expression = Expression(this) greaterThan other
+    public infix fun Int.greaterThan(other: Int): Expression = Expression(this) greaterThan other
+
+    //>=:
+    public infix fun Expression.greaterThanOrEqualTo(other: Expression): Expression =
+        operator(ComparisonOperator.greaterOrEqual, other)
+
+    public infix fun Expression.greaterThanOrEqualTo(other: Int): Expression =
+        greaterThanOrEqualTo(Expression(other.toString()))
+
+    public infix fun Int.greaterThanOrEqualTo(other: Expression): Expression =
+        Expression(toString()) greaterThanOrEqualTo other
+
+    public infix fun Int.greaterThanOrEqualTo(other: Int): Expression =
+        Expression(toString()) greaterThanOrEqualTo other
+
+    //<:
+    public infix fun Expression.lessThan(other: Expression): Expression = operator(ComparisonOperator.less, other)
+    public infix fun Expression.lessThan(other: Int): Expression = lessThan(Expression(other.toString()))
+    public infix fun Int.lessThan(other: Expression): Expression = Expression(toString()) lessThan other
+    public infix fun Int.lessThan(other: Int): Expression = Expression(toString()) lessThan other
+
+    //<=:
+    public infix fun Expression.lessThanOrEqualTo(other: Expression): Expression =
+        operator(ComparisonOperator.lessOrEqual, other)
+
+    public infix fun Expression.lessThanOrEqualTo(other: Int): Expression =
+        greaterThanOrEqualTo(Expression(other.toString()))
+
+    public infix fun Int.lessThanOrEqualTo(other: Expression): Expression =
+        Expression(toString()) lessThanOrEqualTo other
+
+    public infix fun Int.lessThanOrEqualTo(other: Int): Expression =
+        Expression(toString()) lessThanOrEqualTo other
+
+    //and:
+    public infix fun Expression.and(other: Expression): Expression = operator(BooleanOperator.and, other)
+
+    //or:
+    public infix fun Expression.or(other: Expression): Expression = operator(BooleanOperator.or, other)
+
+    /*
+     * [Xpath functions](https://en.wikipedia.org/wiki/XPath#Functions_and_operators)
      */
     public fun LocationPath.compare(operator: ComparisonOperator, other: String): Expression =
         this.toString().compare(operator, other)
