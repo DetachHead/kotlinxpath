e9dee43c059dcfde4cd1cc4b8922341a4391b9a9
DetachHead <detachhead@users.noreply.github.com>
2020-10-10 21:11:20 +1000

    formatting

--- src/commonMain/kotlin/shortcuts.kt
+++ src/commonMain/kotlin/shortcuts.kt
@@ -9,7 +9,7 @@ functions for the shortcut syntax's made available in Xpath
  *
  * short for `/descendant-or-self::components.getNode()/`
  */
-public val LocationPathBuilder.any: LocationPath get() = xpath { components.Axis.`descendant-or-self`(node()) }
+public val LocationPathBuilder.any: LocationPath get() = xpath { Axis.`descendant-or-self`(node()) }
 
 /**
  * equivalent to the `@` shortcut.
@@ -21,18 +21,18 @@ public val LocationPathBuilder.any: LocationPath get() = xpath { components.Axis
  * attr("components.getFoo").toString() == "@components.getFoo"
  * ```
  */
-public fun LocationPathBuilder.attr(name: String): LocationPath = xpath { components.Axis.attribute(name) }
+public fun LocationPathBuilder.attr(name: String): LocationPath = xpath { Axis.attribute(name) }
 
 /**
  * equivalent to the `..` shortcut.
  *
  * short for `parent::components.getNode()`
  */
-public val LocationPathBuilder.parent: LocationPath get() = xpath { components.Axis.parent(node()) }
+public val LocationPathBuilder.parent: LocationPath get() = xpath { Axis.parent(node()) }
 
 /**
  * equivalent to the `.` shortcut.
  *
  * short for `self::components.getNode()`
  */
-public val LocationPathBuilder.self: LocationPath get() = xpath { components.Axis.self(node()) }
+public val LocationPathBuilder.self: LocationPath get() = xpath { Axis.self(node()) }
