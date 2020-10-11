4bc310e1d635d43d1b59ff24ae34e564bde8c365
DetachHead <detachhead@users.noreply.github.com>
2020-10-11 23:34:03 +1000

    add publish config

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("multiplatform") version "1.4.0"
     id("org.jetbrains.dokka") version "1.4.10"
+    `maven-publish`
 }
-group = "org.example"
+group = "io.github.detachhead"
 version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
@@ -45,5 +46,56 @@ kotlin {
 
         @Suppress("unused_variable")
         val mingwX64Test by getting { }
+    }
+    configure(listOf(targets["metadata"], jvm(), js())) {
+        mavenPublication {
+            val targetPublication = this@mavenPublication
+            tasks.withType<AbstractPublishToMaven>()
+                .matching { it.publication == targetPublication }
+                .all { onlyIf { findProperty("isMainHost") == "true" } }
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>(project.name) {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+        }
+        publishing.publications.map {
+            it.name
+        }.find {
+            it != "kotlinMultiplatform"
+        }
+    }
+}
+
+afterEvaluate {
+    configure<PublishingExtension> {
+        publications.all {
+            val mavenPublication = this as? MavenPublication
+            mavenPublication?.artifactId =
+                "${project.name}${"-$name".takeUnless { "kotlinMultiplatform" in name }.orEmpty()}"
+        }
+    }
+}
+
+configure<PublishingExtension> {
+    publications {
+        withType<MavenPublication> {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+        }
+    }
+    repositories {
+        maven("https://api.bintray.com/maven/detachhead/detach/${project.name}/;publish=1") {
+            credentials {
+                username = project.findProperty("bintrayUser").toString()
+                password = project.findProperty("bintrayKey").toString()
+            }
+        }
     }
 }
